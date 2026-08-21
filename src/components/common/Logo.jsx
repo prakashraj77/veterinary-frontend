cc938504fb98c0b84fc244af55c362777454bab4
vasanthzenve <vasanthzenve@gmail.com>
2026-08-21 15:05:24 +0530

    Update Zenve veterinary frontend

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,10 +1,17 @@
 export default function Logo() {
   return (
     <div className="logo">
-      <div className="logo-icon">Z</div>
+      <div className="logo-icon">
+        <img
+          src="/zenve.png"
+          alt="Zenve Logo"
+          className="zenve-logo"
+        />
+      </div>
+
       <div>
-        <h3>VetiCare</h3>
-        <span>Veterinary Doctor</span>
+        <h3>Zenve Doctors</h3>
+        <span>Veterinary Practice OS</span>
       </div>
     </div>
   );
--- src/constants/app.js
+++ src/constants/app.js
@@ -1,5 +1,5 @@
 export const APP = {
-  NAME: "VD Dashboard",
+  NAME: "Zenve Doctors",
 
   VERSION: "1.0.0",
 
